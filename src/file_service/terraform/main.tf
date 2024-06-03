b3900dea6b504fda28c7391fd73c0fbc77aaecc9
sh1un <a0923183408@gmail.com>
2024-06-03 17:37:32 +0800

    feat(terraform): update AWS API Gateway configuration for CORS setting
    SCRUM-16

--- src/file_service/terraform/main.tf
+++ src/file_service/terraform/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
 
 data "aws_region" "current" {}
 
-
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
   assume_role_policy = jsonencode({
@@ -27,14 +26,14 @@ resource "aws_iam_role" "lambda_exec_role" {
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../list_files"
-  output_path = "${path.module}/list_file_function.zip"
+  output_path = "${path.module}/list_files_function.zip"
 }
 
 resource "aws_lambda_function" "list_files" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "list_files"
   role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "list_file_function.lambda_handler"
+  handler          = "list_files_function.lambda_handler"
   source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
   runtime          = "python3.11"
 
@@ -72,8 +71,85 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.list_files.invoke_arn
 }
 
+resource "aws_api_gateway_method_response" "get_files_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.files_api.id
+  resource_id = aws_api_gateway_resource.files_resource.id
+  http_method = aws_api_gateway_method.get_files_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "lambda_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.files_api.id
+  resource_id = aws_api_gateway_resource.files_resource.id
+  http_method = aws_api_gateway_method.get_files_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.files_api.id
+  resource_id   = aws_api_gateway_resource.files_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.files_api.id
+  resource_id = aws_api_gateway_resource.files_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.files_api.id
+  resource_id = aws_api_gateway_resource.files_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.files_api.id
+  resource_id = aws_api_gateway_resource.files_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_response_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 resource "aws_api_gateway_deployment" "files_api_deployment" {
-  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_method_response.get_files_response_200,
+    aws_api_gateway_integration_response.lambda_integration_response_200,
+    aws_api_gateway_method.options_method,
+    aws_api_gateway_method_response.options_response_200,
+    aws_api_gateway_integration.options_integration,
+    aws_api_gateway_integration_response.options_integration_response
+  ]
   rest_api_id = aws_api_gateway_rest_api.files_api.id
   stage_name  = "dev"
 }
