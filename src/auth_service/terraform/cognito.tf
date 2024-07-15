e0be149a38fec14d84c02ee0d7963d81d724da5f
sh1un <a0923183408@gmail.com>
2024-07-15 23:43:05 +0800

    feat(auth_service): add AWS Cognito (SCRUM-44)

---
+++ src/auth_service/terraform/cognito.tf
@@ -0,0 +1,19 @@
+resource "aws_cognito_user_pool" "aws_educate_tpet_cognito_user_pool" {
+  name = "aws_educate_tpet_cognito_user_pool"
+}
+
+resource "aws_cognito_user_pool_client" "aws_educate_tpet_cognito_client" {
+  name                                 = "aws_educate_tpet_cognito_client"
+  allowed_oauth_flows_user_pool_client = true
+  generate_secret                      = false
+  allowed_oauth_scopes                 = ["aws.cognito.signin.user.admin", "email", "openid", "profile"]
+  allowed_oauth_flows                  = ["implicit", "code"]
+  explicit_auth_flows                  = ["ADMIN_NO_SRP_AUTH", "USER_PASSWORD_AUTH"]
+  supported_identity_providers         = ["COGNITO"]
+
+  user_pool_id  = aws_cognito_user_pool.aws_educate_tpet_cognito_user_pool.id
+  callback_urls = ["https://example.com"]
+  logout_urls   = ["https://sumeet.life"]
+}
+
+
