aaf9bf1afcf76bc1cadeb6e488e4302e16d4f051
sh1un <a0923183408@gmail.com>
2024-07-17 06:05:57 +0800

    feat(auth_service): add new api endpoint `GET /users/me` (SCRUM-44)

--- src/auth_service/terraform/api_gateway.tf
+++ src/auth_service/terraform/api_gateway.tf
@@ -89,6 +89,18 @@ module "api_gateway" {
       }
     }
 
+    "GET /users/me" = {
+      detailed_metrics_enabled = true
+      throttling_rate_limit    = 80
+      throttling_burst_limit   = 40
+      integration = {
+        uri                    = module.get_me_lambda.lambda_function_arn # Remember to change
+        type                   = "AWS_PROXY"
+        payload_format_version = "1.0"
+        timeout_milliseconds   = 29000
+      }
+    }
+
 
 
     "$default" = {
--- src/auth_service/terraform/lambda.tf
+++ src/auth_service/terraform/lambda.tf
@@ -25,6 +25,7 @@ locals {
   change_password_function_name_and_ecr_repo_name   = "${var.environment}-${var.service_underscore}-change_password-${random_string.this.result}"
   lambda_authorizer_function_name_and_ecr_repo_name = "${var.environment}-${var.service_underscore}-lambda_authorizer-${random_string.this.result}"
   get_user_function_name_and_ecr_repo_name          = "${var.environment}-${var.service_underscore}-get_user-${random_string.this.result}"
+  get_me_function_name_and_ecr_repo_name            = "${var.environment}-${var.service_underscore}-get_me-${random_string.this.result}"
   path_include                                      = ["**"]
   path_exclude                                      = ["**/__pycache__/**"]
   files_include                                     = setunion([for f in local.path_include : fileset(local.source_path, f)]...)
@@ -464,6 +465,111 @@ module "get_user_docker_image" {
 
   # docker_campaign_path = "${local.source_path}/path/to/Dockercampaign" # set `docker_campaign_path` If your Dockercampaign is not in `source_path`
   source_path = "${local.source_path}/get_user/" # Remember to change
+  triggers = {
+    dir_sha = local.dir_sha
+  }
+
+}
+
+
+####################################
+####################################
+####################################
+# GET /get_me ####################
+####################################
+####################################
+####################################
+
+module "get_me_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "7.7.0"
+
+  function_name  = local.get_me_function_name_and_ecr_repo_name
+  description    = "AWS Educate TPET ${var.service_hyphen} in ${var.environment}: POST /get_me"
+  create_package = false
+  timeout        = 300
+
+  ##################
+  # Container Image
+  ##################
+  package_type  = "Image"
+  architectures = ["x86_64"] # or ["arm64"]
+  image_uri     = module.get_me_docker_image.image_uri
+
+  publish = true # Whether to publish creation/change as new Lambda Function Version.
+
+
+  environment_variables = {
+    "ENVIRONMENT"    = var.environment,
+    "SERVICE"        = var.service_underscore
+    "DYNAMODB_TABLE" = var.dynamodb_table
+  }
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service    = "apigateway"
+      source_arn = "${module.api_gateway.api_execution_arn}/*/*"
+    }
+  }
+
+  tags = {
+    "Terraform"   = "true",
+    "Environment" = var.environment,
+    "Service"     = var.service_underscore
+  }
+  ######################
+  # Additional policies
+  ######################
+
+  attach_policy_statements = true
+  policy_statements = {
+    dynamodb_crud = {
+      effect = "Allow",
+      actions = [
+        "dynamodb:BatchGetItem",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:UpdateItem"
+      ],
+      resources = [
+        "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.this.account_id}:table/${var.dynamodb_table}"
+      ]
+    },
+  }
+}
+
+module "get_me_docker_image" {
+  source  = "terraform-aws-modules/lambda/aws//modules/docker-build"
+  version = "7.7.0"
+
+  create_ecr_repo      = true
+  keep_remotely        = true
+  use_image_tag        = false
+  image_tag_mutability = "MUTABLE"
+  ecr_repo             = local.get_me_function_name_and_ecr_repo_name
+  ecr_repo_lifecycle_policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep only the last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+
+  # docker_campaign_path = "${local.source_path}/path/to/Dockercampaign" # set `docker_campaign_path` If your Dockercampaign is not in `source_path`
+  source_path = "${local.source_path}/get_me/" # Remember to change
   triggers = {
     dir_sha = local.dir_sha
   }
