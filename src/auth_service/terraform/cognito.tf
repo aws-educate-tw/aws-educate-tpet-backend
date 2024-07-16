9986521d584bc11ff1cc11786e4447958f929da1
sh1un <a0923183408@gmail.com>
2024-07-16 23:24:22 +0800

    feat(cognito): add password policy (SCRUM-44)

--- src/auth_service/terraform/cognito.tf
+++ src/auth_service/terraform/cognito.tf
@@ -1,12 +1,21 @@
 resource "aws_cognito_user_pool" "aws_educate_tpet_cognito_user_pool" {
-  name              = "aws_educate_tpet_cognito_user_pool"
+  name              = "${var.environment}-aws_educate_tpet_cognito_user_pool"
   alias_attributes  = ["email"]
   mfa_configuration = "OPTIONAL"
   software_token_mfa_configuration {
     enabled = true
   }
-}
 
+  # Set password policy to not require password change on first login
+  password_policy {
+    minimum_length                   = 6
+    require_lowercase                = false
+    require_numbers                  = false
+    require_symbols                  = false
+    require_uppercase                = false
+    temporary_password_validity_days = 365
+  }
+}
 
 resource "aws_cognito_user_pool_client" "aws_educate_tpet_cognito_client" {
   name                                 = "${var.environment}-aws_educate_tpet_cognito_client"
@@ -17,22 +26,7 @@ resource "aws_cognito_user_pool_client" "aws_educate_tpet_cognito_client" {
   explicit_auth_flows                  = ["ADMIN_NO_SRP_AUTH", "USER_PASSWORD_AUTH"]
   supported_identity_providers         = ["COGNITO"]
 
-
   user_pool_id  = aws_cognito_user_pool.aws_educate_tpet_cognito_user_pool.id
   callback_urls = ["https://tpet.aws-educate.tw"]
   logout_urls   = ["https://tpet.aws-educate.tw"]
 }
-
-# resource "aws_cognito_user_pool_domain" "domain" {
-#   domain       = "educate-tpet-cognito"
-#   user_pool_id = aws_cognito_user_pool.aws_educate_tpet_cognito_user_pool.id
-# }
-
-
-# resource "aws_cognito_user_pool_ui_customization" "hosted_ui" {
-#   client_id = aws_cognito_user_pool_client.aws_educate_tpet_cognito_client.id
-
-#   css = ".label-customizable {font-weight: 400;}"
-
-#   user_pool_id = aws_cognito_user_pool.aws_educate_tpet_cognito_user_pool.id
-# }
