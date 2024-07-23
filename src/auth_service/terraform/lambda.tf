f489b59f7ac4f7a4886cb988533b7d7bd6406625
sh1un <a0923183408@gmail.com>
2024-07-24 03:26:05 +0800

    chore(auth_service): remove lambda authorizer (SCRUM-98)

--- src/auth_service/terraform/lambda.tf
+++ src/auth_service/terraform/lambda.tf
@@ -20,19 +20,18 @@ resource "random_string" "this" {
 }
 
 locals {
-  source_path                                       = "${path.module}/.."
-  login_function_name_and_ecr_repo_name             = "${var.environment}-${var.service_underscore}-login-${random_string.this.result}"
-  change_password_function_name_and_ecr_repo_name   = "${var.environment}-${var.service_underscore}-change_password-${random_string.this.result}"
-  lambda_authorizer_function_name_and_ecr_repo_name = "${var.environment}-${var.service_underscore}-lambda_authorizer-${random_string.this.result}"
-  get_user_function_name_and_ecr_repo_name          = "${var.environment}-${var.service_underscore}-get_user-${random_string.this.result}"
-  get_me_function_name_and_ecr_repo_name            = "${var.environment}-${var.service_underscore}-get_me-${random_string.this.result}"
-  is_logged_in_function_name_and_ecr_repo_name      = "${var.environment}-${var.service_underscore}-is_logged_in-${random_string.this.result}"
-  path_include                                      = ["**"]
-  path_exclude                                      = ["**/__pycache__/**"]
-  files_include                                     = setunion([for f in local.path_include : fileset(local.source_path, f)]...)
-  files_exclude                                     = setunion([for f in local.path_exclude : fileset(local.source_path, f)]...)
-  files                                             = sort(setsubtract(local.files_include, local.files_exclude))
-  dir_sha                                           = sha1(join("", [for f in local.files : filesha1("${local.source_path}/${f}")]))
+  source_path                                     = "${path.module}/.."
+  login_function_name_and_ecr_repo_name           = "${var.environment}-${var.service_underscore}-login-${random_string.this.result}"
+  change_password_function_name_and_ecr_repo_name = "${var.environment}-${var.service_underscore}-change_password-${random_string.this.result}"
+  get_user_function_name_and_ecr_repo_name        = "${var.environment}-${var.service_underscore}-get_user-${random_string.this.result}"
+  get_me_function_name_and_ecr_repo_name          = "${var.environment}-${var.service_underscore}-get_me-${random_string.this.result}"
+  is_logged_in_function_name_and_ecr_repo_name    = "${var.environment}-${var.service_underscore}-is_logged_in-${random_string.this.result}"
+  path_include                                    = ["**"]
+  path_exclude                                    = ["**/__pycache__/**"]
+  files_include                                   = setunion([for f in local.path_include : fileset(local.source_path, f)]...)
+  files_exclude                                   = setunion([for f in local.path_exclude : fileset(local.source_path, f)]...)
+  files                                           = sort(setsubtract(local.files_include, local.files_exclude))
+  dir_sha                                         = sha1(join("", [for f in local.files : filesha1("${local.source_path}/${f}")]))
 }
 
 provider "docker" {
@@ -255,118 +254,6 @@ module "change_password_docker_image" {
 }
 
 
-
-####################################
-####################################
-####################################
-# Lambda Authorizer ################
-####################################
-####################################
-####################################
-
-module "lambda_authorizer_lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "7.7.0"
-
-  function_name  = local.lambda_authorizer_function_name_and_ecr_repo_name
-  description    = "AWS Educate TPET ${var.service_hyphen} in ${var.environment}: API Gateway custom Authorizer"
-  create_package = false
-  timeout        = 300
-
-  ##################
-  # Container Image
-  ##################
-  package_type  = "Image"
-  architectures = ["x86_64"] # or ["arm64"]
-  image_uri     = module.lambda_authorizer_docker_image.image_uri
-
-  publish = true # Whether to publish creation/change as new Lambda Function Version.
-
-
-  environment_variables = {
-    "ENVIRONMENT"          = var.environment,
-    "SERVICE"              = var.service_underscore
-    "CURRENT_AWS_REGION"   = var.aws_region
-    "COGNITO_USER_POOL_ID" = data.aws_ssm_parameter.aws_educate_tpet_cognito_user_pool_id.value
-    "COGNITO_CLIENT_ID"    = data.aws_ssm_parameter.aws_educate_tpet_cognito_client_id.value
-  }
-
-
-
-  allowed_triggers = {
-    AllowExecutionFromAPIGateway = {
-      service    = "apigateway"
-      source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.this.account_id}:*/*/*/*"
-    }
-    AllowExecutionFromAPIGatewayAuthorizer = {
-      service    = "apigateway"
-      source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.this.account_id}:*/authorizers/*"
-    }
-  }
-
-  tags = {
-    "Terraform"   = "true",
-    "Environment" = var.environment,
-    "Service"     = var.service_underscore
-  }
-  ######################
-  # Additional policies
-  ######################
-
-  attach_policy_statements = true
-  policy_statements = {
-    dynamodb_crud = {
-      effect = "Allow",
-      actions = [
-        "dynamodb:BatchGetItem",
-        "dynamodb:BatchWriteItem",
-        "dynamodb:DeleteItem",
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:Query",
-        "dynamodb:Scan",
-        "dynamodb:UpdateItem"
-      ],
-      resources = [
-        "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.this.account_id}:table/${var.dynamodb_table}"
-      ]
-    }
-  }
-}
-
-module "lambda_authorizer_docker_image" {
-  source  = "terraform-aws-modules/lambda/aws//modules/docker-build"
-  version = "7.7.0"
-
-  create_ecr_repo      = true
-  keep_remotely        = true
-  use_image_tag        = false
-  image_tag_mutability = "MUTABLE"
-  ecr_repo             = local.lambda_authorizer_function_name_and_ecr_repo_name
-  ecr_repo_lifecycle_policy = jsonencode({
-    "rules" : [
-      {
-        "rulePriority" : 1,
-        "description" : "Keep only the last 10 images",
-        "selection" : {
-          "tagStatus" : "any",
-          "countType" : "imageCountMoreThan",
-          "countNumber" : 10
-        },
-        "action" : {
-          "type" : "expire"
-        }
-      }
-    ]
-  })
-
-  # docker_campaign_path = "${local.source_path}/path/to/Dockercampaign" # set `docker_campaign_path` If your Dockercampaign is not in `source_path`
-  source_path = "${local.source_path}/lambda_authorizer/" # Remember to change
-  triggers = {
-    dir_sha = local.dir_sha
-  }
-
-}
 
 ####################################
 ####################################
--- src/auth_service/terraform/ssm.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ssm_parameter" "lambda_authorizer_lambda_invoke_arn" {
-  name  = "${var.environment}-lambda_authorizer_lambda_invoke_arn"
-  type  = "String"
-  value = module.lambda_authorizer_lambda.lambda_function_invoke_arn
-}
-
