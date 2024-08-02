8883e3b00c50e1bce554ac85e2c866418688922d
sh1un <a0923183408@gmail.com>
2024-08-02 12:39:26 +0800

    feat(email_service/validate_input): add sufficient permissions to Lambda and add data_util.py for resolving decimals (SCRUM-108)

--- src/email_service/terraform/lambda.tf
+++ src/email_service/terraform/lambda.tf
@@ -111,6 +111,23 @@ module "validate_input_lambda" {
       resources = [
         "arn:aws:sqs:${var.aws_region}:${data.aws_caller_identity.this.account_id}:${module.send_email_sqs.queue_name}"
       ]
+    },
+    dynamodb_crud = {
+      effect = "Allow",
+      actions = [
+        "dynamodb:BatchGetItem",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:UpdateItem"
+      ],
+      resources = [
+        "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.this.account_id}:table/${var.run_dynamodb_table}",
+        "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.this.account_id}:table/${var.run_dynamodb_table}/index/*"
+      ]
     }
   }
 }
