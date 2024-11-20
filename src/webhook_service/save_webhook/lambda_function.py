92348f7aa119c01afd1dabe765fd1ce2ed0a7a92
chungchihhan <harryup2000@gmail.com>
2024-11-21 00:32:03 +0800

    feat(webhook_service/save_webhook): save generated webhook_url into dynamoDB (SCRUM-162)

--- src/webhook_service/save_webhook/lambda_function.py
+++ src/webhook_service/save_webhook/lambda_function.py
@@ -2,11 +2,15 @@ import json
 import os
 from decimal import Decimal
 import uuid
-
+import logging
 import boto3
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.getenv("DYNAMODB_TABLE"))
+trigger_webhook_api_endpoint = os.getenv("TRIGGER_WEBHOOK_API_ENDPOINT")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
@@ -17,15 +21,13 @@ class DecimalEncoder(json.JSONEncoder):
 
 def lambda_handler(event, context):
     try:
-        # Parse incoming data
         data = json.loads(event['body'])
-        
-        # Generate a unique webhook_id
         webhook_id = str(uuid.uuid4())
-        
-        # Prepare the item to be inserted into DynamoDB
+        webhook_url = f"{trigger_webhook_api_endpoint}/{webhook_id}"
+
         item = {
-            "webhook_id": webhook_id,  # Partition key
+            "webhook_id": webhook_id,
+            "webhook_url": webhook_url,
             "subject": data.get("subject"),
             "display_name": data.get("display_name"),
             "template_file_id": data.get("template_file_id"),
@@ -39,10 +41,11 @@ def lambda_handler(event, context):
             "hask_key": data.get("hask_key"),
             "iv_key": data.get("iv_key"),
             "webhook_name": data.get("webhook_name")
-        }        
-        # Insert the item into DynamoDB
+        }
+
+        logger.info("Attempting to put item into DynamoDB: %s", item)
         response = table.put_item(Item=item)
-        trigger_webhook_api_endpoint = os.getenv("TRIGGER_WEBHOOK_API_ENDPOINT")
+        logger.info("DynamoDB put_item response: %s", response)
 
         return {
             "statusCode": 200,
@@ -55,7 +58,7 @@ def lambda_handler(event, context):
                     "status": "SUCCESS",
                     "message": "Webhook successfully created.",
                     "webhook_id": webhook_id,
-                    "webhook_url": f"{trigger_webhook_api_endpoint}/{webhook_id}",
+                    "webhook_url": webhook_url,
                 }, cls=DecimalEncoder),
         }
     except Exception as e:
