749046e8cdeea2fbe3d2d2eb8f60b7da2e359d63
sh1un <a0923183408@gmail.com>
2025-01-01 23:55:19 +0800

    feat(file_service): add prewarm request handling to Lambda functions and update Terraform configurations (SCRUM-199)

--- src/file_service/get_file/lambda_function.py
+++ src/file_service/get_file/lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from decimal import Decimal
 
@@ -6,6 +7,10 @@ import boto3
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.getenv("DYNAMODB_TABLE"))
+
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class DecimalEncoder(json.JSONEncoder):
@@ -16,6 +21,12 @@ class DecimalEncoder(json.JSONEncoder):
 
 
 def lambda_handler(event, context):
+
+    # Identify if the incoming event is a prewarm request
+    if event.get("action") == "PREWARM":
+        logger.info("Received a prewarm request. Skipping business logic.")
+        return {"statusCode": 200, "body": "Successfully warmed up"}
+
     file_id = event["pathParameters"]["file_id"]
     print("test")
     response = table.get_item(Key={"file_id": file_id})
