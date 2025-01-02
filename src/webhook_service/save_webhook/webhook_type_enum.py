c9333b9ad0b0ae3916c9c4c51a65ec24c0b09e1d
chungchihhan <harryup2000@gmail.com>
2025-01-02 10:50:54 +0800

    refactor(webhook_service): unify the webhook type naming style (SCRUM-157)

--- src/webhook_service/save_webhook/webhook_type_enum.py
+++ src/webhook_service/save_webhook/webhook_type_enum.py
@@ -1,6 +1,18 @@
+"""
+This module defines an enumeration for different webhook types,
+such as SurveyCake and Slack, for use in integrations.
+"""
+
 from enum import Enum
 
 
-class WebhookType(Enum):  
-    SURVEY_CAKE = "survey_cake"  
-    SLACK = "slack"  
+class WebhookType(Enum):
+    """
+    Enumeration of supported webhook types.
+
+    Attributes:
+        SURVEYCAKE: Represents a webhook for SurveyCake.
+        SLACK: Represents a webhook for Slack.
+    """
+    SURVEYCAKE = "surveycake"
+    SLACK = "slack"
