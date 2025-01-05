5d94fe543b44dde029a4f2d3d65a463d5f6dbff3
chungchihhan <harryup2000@gmail.com>
2025-01-05 15:53:35 +0800

    fix(webhook_service): fix the error of docker not deploying with github action (SCRUM-214)

--- src/webhook_service/terraform/lambda.tf
+++ src/webhook_service/terraform/lambda.tf
@@ -25,7 +25,7 @@ locals {
 }
 
 provider "docker" {
-  host = var.environment == "local-dev" ? var.docker_host : null
+  host = var.environment == "local-dev" ? var.docker_host : "unix:///var/run/docker.sock"
 
   registry_auth {
     address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, var.aws_region)
