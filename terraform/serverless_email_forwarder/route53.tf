6b2b83379957f91e34f8de5ec007abbdf0ce63de
ki225 <271yeye@gmail.com>
2025-02-22 20:52:58 +0800

    feat(terraform): MX route53 record (SCRUM-127)

---
+++ terraform/serverless_email_forwarder/route53.tf
@@ -0,0 +1,32 @@
+# module "zones" {
+#   source  = "terraform-aws-modules/route53/aws//modules/zones"
+#   version = "~> 3.0"
+
+#   zones = {
+#     "aws-educate.tw" = {
+#       comment = "aws-educate.tw"
+#     }
+#   }
+
+#   tags = {
+#     ManagedBy = "Terraform"
+#   }
+# }
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 3.0"
+
+#   zone_name = keys(module.zones.route53_zone_zone_id)[0]
+  zone_id = var.zone_id
+  
+  records = [
+    {
+      name    = ""
+      type    = "MX"
+      ttl     = 300
+      records = ["10 inbound-smtp.ap-northeast-1.amazonaws.com"]
+    }
+  ]
+  depends_on = [module.zones]
+}
