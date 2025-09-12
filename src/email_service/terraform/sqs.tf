2c43a5fb850e427f886cc1ad85e7e77977f7732f
ki225 <271yeye@gmail.com>
2025-09-12 15:31:17 +0800

    feat(email_service/send_email): add 2 sqs for auto-resume  (SCRUM-369)

--- src/email_service/terraform/sqs.tf
+++ src/email_service/terraform/sqs.tf
@@ -28,3 +28,35 @@ module "send_email_sqs" {
     maxReceiveCount = 2
   }
 }
+
+
+module "auto_resumer_sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.2.0"
+
+  name                       = "${var.environment}-auto-resumer-sqs"
+  visibility_timeout_seconds = 3600 # Second, make sure it is larger than the Lambda timeout
+
+  # Dead letter queue
+  create_dlq = true
+  redrive_policy = {
+    # One failure to receive a message would cause the message to move to the DLQ
+    maxReceiveCount = 2
+  }
+}
+
+
+module "upsert_run_sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.2.0"
+
+  name                       = "${var.environment}-upsert-run-sqs"
+  visibility_timeout_seconds = 3600 # Second, make sure it is larger than the Lambda timeout
+
+  # Dead letter queue
+  create_dlq = true
+  redrive_policy = {
+    # One failure to receive a message would cause the message to move to the DLQ
+    maxReceiveCount = 2
+  }
+}
