13f2959996bed9bfcf87c6e6210d302775d52873
ki225 <271yeye@gmail.com>
2025-11-14 21:59:18 +0800

    feat(forward_email): forwarding email terraform iac setting (SCRUM-127)

---
+++ src/forward_email/terraform/ses.tf
@@ -0,0 +1,147 @@
+resource "aws_ses_domain_identity" "ses_aws_educate_tpet_domain" {
+  domain = var.domain_name
+}
+
+# SES - Email Indentity
+resource "aws_ses_email_identity" "ses_aws_educate_tpet_email" {
+  email = var.ses_email_identity
+}
+
+resource "aws_ses_receipt_rule_set" "ses_receipt_rule_set" {
+  rule_set_name = "forward_email"
+}
+
+# Add a header to the email and store it in S3
+resource "aws_ses_receipt_rule" "ses_receipt_rule_forward_to_mkt" {
+  name          = "forward_to_mkt" # rule name
+  rule_set_name = aws_ses_receipt_rule_set.ses_receipt_rule_set.rule_set_name
+  recipients    = [var.mkt_email]
+  enabled       = true #  enabled receipt rules within the active rule set.
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = var.bucket_name
+    object_key_prefix = "mkt/"
+    position    = 1
+  }
+
+  depends_on = [
+    aws_s3_bucket.aws_educate_tpet_email_bucket,
+    aws_s3_bucket_policy.ses_put_object
+  ]
+}
+
+resource "aws_ses_receipt_rule" "ses_receipt_rule_forward_to_dev" {
+  name          = "forward_to_dev" # rule name
+  rule_set_name = aws_ses_receipt_rule_set.ses_receipt_rule_set.rule_set_name
+  recipients    = [var.dev_email]
+  enabled       = true #  enabled receipt rules within the active rule set.
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = var.bucket_name
+    object_key_prefix = "dev/"
+    position    = 1
+  }
+
+  depends_on = [
+    aws_s3_bucket.aws_educate_tpet_email_bucket,
+    aws_s3_bucket_policy.ses_put_object
+  ]
+}
+
+resource "aws_ses_receipt_rule" "ses_receipt_rule_forward_to_event" {
+  name          = "forward_to_event" # rule name
+  rule_set_name = aws_ses_receipt_rule_set.ses_receipt_rule_set.rule_set_name
+  recipients    = [var.event_email]
+  enabled       = true #  enabled receipt rules within the active rule set.
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = var.bucket_name
+    object_key_prefix = "event/"
+    position    = 1
+  }
+
+  depends_on = [
+    aws_s3_bucket.aws_educate_tpet_email_bucket,
+    aws_s3_bucket_policy.ses_put_object
+  ]
+}
+
+resource "aws_ses_receipt_rule" "ses_receipt_rule_forward_to_group1" {
+  name          = "forward_to_group1" # rule name
+  rule_set_name = aws_ses_receipt_rule_set.ses_receipt_rule_set.rule_set_name
+  recipients    = [var.group1_email]
+  enabled       = true #  enabled receipt rules within the active rule set.
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = var.bucket_name
+    object_key_prefix = "group1/"
+    position    = 1
+  }
+
+  depends_on = [
+    aws_s3_bucket.aws_educate_tpet_email_bucket,
+    aws_s3_bucket_policy.ses_put_object
+  ]
+}
+
+resource "aws_ses_receipt_rule" "ses_receipt_rule_forward_to_group2" {
+  name          = "forward_to_group2" # rule name
+  rule_set_name = aws_ses_receipt_rule_set.ses_receipt_rule_set.rule_set_name
+  recipients    = [var.group2_email]
+  enabled       = true #  enabled receipt rules within the active rule set.
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = var.bucket_name
+    object_key_prefix = "group2/"
+    position    = 1
+  }
+
+  depends_on = [
+    aws_s3_bucket.aws_educate_tpet_email_bucket,
+    aws_s3_bucket_policy.ses_put_object
+  ]
+}
+
+
+resource "aws_ses_receipt_rule" "ses_receipt_rule_forward_to_group3" {
+  name          = "forward_to_group3" # rule name
+  rule_set_name = aws_ses_receipt_rule_set.ses_receipt_rule_set.rule_set_name
+  recipients    = [var.group3_email]
+  enabled       = true #  enabled receipt rules within the active rule set.
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = var.bucket_name
+    object_key_prefix = "group3/"
+    position    = 1
+  }
+
+  depends_on = [
+    aws_s3_bucket.aws_educate_tpet_email_bucket,
+    aws_s3_bucket_policy.ses_put_object
+  ]
+}
+
+resource "aws_ses_receipt_rule" "ses_receipt_rule_forward_to_default" {
+  name          = "forward_to_default" # rule name
+  rule_set_name = aws_ses_receipt_rule_set.ses_receipt_rule_set.rule_set_name
+  recipients    = [var.aws_educate_domain, var.aws_educate_domain_dot_prefix]
+  enabled       = true #  enabled receipt rules within the active rule set.
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = var.bucket_name
+    object_key_prefix = "default/"
+    position    = 1
+  }
+
+  depends_on = [
+    aws_s3_bucket.aws_educate_tpet_email_bucket,
+    aws_s3_bucket_policy.ses_put_object
+  ]
+}
---
+++ src/forward_email/terraform/variables.tf
@@ -0,0 +1,95 @@
+variable "aws_region" {
+  description = "aws region"
+}
+
+variable "environment" {
+  description = "Current environtment: prod(ap-northeast-1)/dev(us-east-1)/local-dev(us-west-2), default dev(us-east-1)"
+}
+
+variable "domain_name" {
+  description = "Domain name, for example: example.com"
+  default     = "aws-educate.tw"
+}
+
+variable "docker_host" {
+  description = "Docker host"
+  type        = string
+}
+
+variable "lambda_architecture" {
+  description = "CPU architecture for container image"
+  type    = string
+  default = "x86_64"
+}
+
+variable "ses_email_identity" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "awseducate.cloudambassador@gmail.com"
+}
+
+variable "aws_educate_domain" {
+  description = "aws-educate.tw"
+  type        = string
+  default     = "aws-educate.tw"
+}
+
+variable "aws_educate_domain_dot_prefix" {
+  description = ".aws-educate.tw"
+  type        = string
+  default     = ".aws-educate.tw"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "aws-educate-tpet-email-forward-bucket"
+}
+
+variable "dev_email" {
+  description = "The email address for dev"
+  type        = string
+  default     = "dev@aws-educate.tw"
+}
+
+variable "mkt_email" {
+  description = "The email address for mkt"
+  type        = string
+  default     = "mkt@aws-educate.tw"
+}
+
+variable "tech_email" {
+  description = "The email address for tech"
+  type        = string
+  default     = "tech@aws-educate.tw"
+}
+
+variable "event_email" {
+  description = "The email address for event"
+  type        = string
+  default     = "event@aws-educate.tw"
+}
+
+variable "group1_email" {
+  description = "The email address for group1"
+  type        = string
+  default     = "group1@aws-educate.tw"
+}
+
+variable "group2_email" {
+  description = "The email address for group2"
+  type        = string
+  default     = "group2@aws-educate.tw"
+}
+
+variable "group3_email" {
+  description = "The email address for group3"
+  type        = string
+  default     = "group3@aws-educate.tw"
+}
+
+variable "zone_id" {
+  description = "The Route 53 Hosted Zone ID for the domain"
+  type        = string
+  default     = "Z07729212EE8WFR3NG5K0"
+}
