3d081d10f5156c113171009b817f0a24264a2f26
Shiun <85695943+sh1un@users.noreply.github.com>
2025-11-30 18:05:06 +0800

    Merge pull request #124 from aws-educate-tw/SCRUM-375-migration-to-new-aws-account
    [SCRUM-375, SCRUM-417] migration to new aws account

--- src/webhook_service/terraform/lambda.tf
+++ src/webhook_service/terraform/lambda.tf
@@ -27,7 +27,6 @@ locals {
 }
 
 provider "docker" {
-  host = var.docker_host
 
   registry_auth {
     address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, var.aws_region)
