8513a3dc921e7d18a732c109f41d5f9e479d96b7
ki225 <271yeye@gmail.com>
2025-12-14 23:30:44 +0800

    fix: use new s3 name (SCRUM-482)

--- terraform/modules/public_s3/main.tf
+++ terraform/modules/public_s3/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "aws_educate_tpet_storage" {
+resource "aws_s3_bucket" "aws_educate_tpet_bucket" {
   bucket = "${var.environment}-aws-educate-tpet-bucket"
 
   tags = {
@@ -8,15 +8,15 @@ resource "aws_s3_bucket" "aws_educate_tpet_storage" {
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.aws_educate_tpet_storage
+  bucket = aws_s3_bucket.aws_educate_tpet_bucket
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-resource "aws_s3_bucket_policy" "aws_educate_tpet_storage_policy" {
-  bucket = aws_s3_bucket.aws_educate_tpet_storage.id
+resource "aws_s3_bucket_policy" "aws_educate_tpet_bucket_policy" {
+  bucket = aws_s3_bucket.aws_educate_tpet_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_policy" "aws_educate_tpet_storage_policy" {
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.aws_educate_tpet_storage.arn}/*"
+        Resource  = "${aws_s3_bucket.aws_educate_tpet_bucket.arn}/*"
       }
     ]
   })
--- terraform/storage/s3/s3.tf
+++ terraform/storage/s3/s3.tf
@@ -11,7 +11,7 @@ locals {
   private_bucket_name = "${var.environment}-aws-educate-tpet-private-bucket"
 }
 
-resource "aws_s3_bucket" "aws_educate_tpet_storage" {
+resource "aws_s3_bucket" "aws_educate_tpet_bucket" {
   bucket = local.public_bucket_name
 
   tags = {
@@ -21,15 +21,15 @@ resource "aws_s3_bucket" "aws_educate_tpet_storage" {
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.aws_educate_tpet_storage.bucket
+  bucket = aws_s3_bucket.aws_educate_tpet_bucket.bucket
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-resource "aws_s3_bucket_policy" "aws_educate_tpet_storage_policy" {
-  bucket = aws_s3_bucket.aws_educate_tpet_storage.id
+resource "aws_s3_bucket_policy" "aws_educate_tpet_bucket_policy" {
+  bucket = aws_s3_bucket.aws_educate_tpet_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -38,13 +38,13 @@ resource "aws_s3_bucket_policy" "aws_educate_tpet_storage_policy" {
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.aws_educate_tpet_storage.arn}/*"
+        Resource  = "${aws_s3_bucket.aws_educate_tpet_bucket.arn}/*"
       }
     ]
   })
 }
-resource "aws_s3_bucket_cors_configuration" "aws_educate_tpet_storage_cors" {
-  bucket = aws_s3_bucket.aws_educate_tpet_storage.id
+resource "aws_s3_bucket_cors_configuration" "aws_educate_tpet_bucket_cors" {
+  bucket = aws_s3_bucket.aws_educate_tpet_bucket.id
 
   cors_rule {
     allowed_headers = ["*"]
