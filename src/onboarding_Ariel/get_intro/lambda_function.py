0914734ca28d6d6638e5b0049fc8a5de82db6f61
arielyu <arielyu999@gmail.com>
2025-12-26 17:31:26 +0800

    test: sync api and test collection for CI pipeline

---
+++ src/onboarding_Ariel/get_intro/lambda_function.py
@@ -0,0 +1,20 @@
+import json
+
+def lambda_handler(event, context):
+    """
+    AWS Lambda 處理器函式
+    """
+    # 這是你要回傳的內容
+    response_body = {
+        "name": "Ariel",
+        "self_intro": "Hi! I'm Ariel, part of the 8th Tech cohort, and I will be joining the TPET team as a backend developer. I enjoy listening to K-pop and playing the piano. Looking forward to working with everyone!"
+    }
+
+    # API Gateway 要求的回傳格式必須包含 statusCode 和 body
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(response_body, ensure_ascii=False)
+    }
