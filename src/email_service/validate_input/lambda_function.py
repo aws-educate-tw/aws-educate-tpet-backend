52506dd09aabff544362e530a314d58a504e0dea
chungchihhan <harryup2000@gmail.com>
2026-01-18 14:35:44 +0800

    fix(email_service): fix the linting error by using X|Y  (SCRUM-222)

--- src/email_service/validate_input/lambda_function.py
+++ src/email_service/validate_input/lambda_function.py
@@ -62,7 +62,7 @@ class ErrorResponder:
             # Convert enum to string if needed
             error_code_str = (
                 error_code.value
-                if isinstance(error_code, (SystemErrorCode, ValidationErrorCode))
+                if isinstance(error_code, SystemErrorCode | ValidationErrorCode)
                 else error_code
             )
             error_body["error_code"] = error_code_str
