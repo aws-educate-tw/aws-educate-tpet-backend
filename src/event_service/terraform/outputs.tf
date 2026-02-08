92eee41bbd5a30ac20f17c8d1a04dd9f35263a56
陳敬翰 <asd1111zxcv@gmail.com>
2026-02-08 14:44:06 +0800

    chore(event_service/terraform): remove unused outputs (SCRUM-538)

--- src/event_service/terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "eventbridge_scheduler_role_arn" {
-  description = "IAM role ARN assumed by EventBridge Scheduler to invoke SyncAuroraLambda"
-  value       = aws_iam_role.eventbridge_scheduler_role.arn
-}
