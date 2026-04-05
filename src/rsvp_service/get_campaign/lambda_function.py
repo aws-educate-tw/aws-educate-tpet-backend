c945d463960593864d946c446d4f701c74999e3c
45678XYZ <aaronwayway@gmail.com>
2026-04-06 00:58:17 +0800

    fix(rsvp_service): remove default value for ENVIRONMENT variable (SCRUM-571)

--- src/rsvp_service/get_campaign/lambda_function.py
+++ src/rsvp_service/get_campaign/lambda_function.py
@@ -18,7 +18,7 @@ dynamodb = boto3.resource("dynamodb")
 CAMPAIGNS_TABLE = os.getenv("CAMPAIGNS_TABLE", "campaign")
 RUNS_CAMPAIGNS_MAPPING_TABLE = os.getenv("RUNS_CAMPAIGNS_MAPPING_TABLE", "campaign_run")
 PARTICIPANTS_TABLE = os.getenv("PARTICIPANTS_TABLE", "participant")
-ENVIRONMENT = os.getenv("ENVIRONMENT", "local_dev")
+ENVIRONMENT = os.getenv("ENVIRONMENT")
 DOMAIN_NAME = os.getenv("DOMAIN_NAME")
 EMAIL_SERVICE_BASE_URL = (
     f"https://{ENVIRONMENT}-email-service-internal-api-tpet.{DOMAIN_NAME}/{ENVIRONMENT}"
