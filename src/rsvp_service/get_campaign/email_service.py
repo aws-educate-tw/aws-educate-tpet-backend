62d5f1903daa36aafc568bebc97623548f299ec4
45678XYZ <aaronwayway@gmail.com>
2026-04-06 22:40:50 +0800

    chore(rsvp_service): move environment variables to global scope (SCRUM-571)

--- src/rsvp_service/get_campaign/email_service.py
+++ src/rsvp_service/get_campaign/email_service.py
@@ -7,14 +7,15 @@ from urllib.error import HTTPError, URLError
 
 logger = logging.getLogger(__name__)
 
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+DOMAIN_NAME = os.getenv("DOMAIN_NAME")
+
 
 class EmailService:
     """Service class for interacting with the email service API."""
 
     def __init__(self, authorization_header=None):
-        environment = os.getenv("ENVIRONMENT")
-        domain_name = os.getenv("DOMAIN_NAME")
-        self.base_url = f"https://{environment}-email-service-internal-api-tpet.{domain_name}/{environment}"
+        self.base_url = f"https://{ENVIRONMENT}-email-service-internal-api-tpet.{DOMAIN_NAME}/{ENVIRONMENT}"
         self.authorization_header = authorization_header
 
     def fetch_campaign_runs(self, campaign_id, max_retries=4, initial_retry_delay=2):
