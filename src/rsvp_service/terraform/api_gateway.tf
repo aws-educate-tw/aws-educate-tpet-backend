d13ba7aed61fd35999f05ba3575d3f163cbe798b
arielyu <arielyu999@gmail.com>
2026-06-05 14:03:01 +0800

    Remove function

--- src/rsvp_service/terraform/api_gateway.tf
+++ src/rsvp_service/terraform/api_gateway.tf
@@ -202,7 +202,7 @@ module "api_gateway" {
 
     "PATCH /campaigns/{campaign_id}" = {
       detailed_metrics_enabled = true
-      throttling_rate_limit    = 80
+      throttling_rate_limit    = 806
       throttling_burst_limit   = 40
 
       # TODO: Re-enable authorizer after auth flow is ready
@@ -217,20 +217,6 @@ module "api_gateway" {
       }
     }
 
-    "PATCH /rsvp/{campaign_id}" = {
-      detailed_metrics_enabled = true
-      throttling_rate_limit    = 80
-      throttling_burst_limit   = 40
-
-      authorization_type = "AWS_IAM"  # 註：如果妳們要用 IAM 驗證就維持 AWS_IAM，如果不用驗證就寫 "NONE"
-      
-      integration = {
-        uri                    = module.update_campaign_lambda.lambda_function_arn # ⚠️ 這裡要改成妳這支 Lambda 的名字
-        type                   = "AWS_PROXY"
-        payload_format_version = "1.0"
-        timeout_milliseconds   = 29000
-      }
-    }
   }
 
   tags = local.tags
